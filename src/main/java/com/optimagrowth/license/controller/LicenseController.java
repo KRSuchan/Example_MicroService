c256471fab14c3ae1f451f484cb845ae89ced66b
SuChan Lee <lsc1814@naver.com>
2024-02-08 17:02:53 +0900

    apply HATEOAS

--- src/main/java/com/optimagrowth/license/controller/LicenseController.java
+++ src/main/java/com/optimagrowth/license/controller/LicenseController.java
@@ -2,12 +2,16 @@ package com.optimagrowth.license.controller;
 
 import com.optimagrowth.license.model.License;
 import com.optimagrowth.license.service.LicenseService;
-import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Locale;
+
+import org.springframework.hateoas.Link;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping(value = "v1/organization/{organizationId}/license")
@@ -19,10 +23,29 @@ public class LicenseController {
     public ResponseEntity<License> getLicense(
             @PathVariable("organizationId") String organizationId,
             @PathVariable("licenseId") String licenseId ) {
+
         License license = licenseService.getLicense(licenseId, organizationId);
+
+        Link selfLink = WebMvcLinkBuilder.linkTo(methodOn(LicenseController.class)
+                        .getLicense(organizationId, licenseId))
+                .withSelfRel();
+
+        Link createLicenseLink = WebMvcLinkBuilder.linkTo(methodOn(LicenseController.class)
+                        .createLicense(organizationId, license, null))
+                .withRel("createLicense");
+
+        Link updateLicenseLink = WebMvcLinkBuilder.linkTo(methodOn(LicenseController.class)
+                        .updateLicense(organizationId, license))
+                .withRel("updateLicense");
+
+        Link deleteLicenseLink = WebMvcLinkBuilder.linkTo(methodOn(LicenseController.class)
+                        .deleteLicense(organizationId, license.getLicenseId()))
+                .withRel("deleteLicense");
+
+        license.add(selfLink, createLicenseLink, updateLicenseLink, deleteLicenseLink);
+
         return ResponseEntity.ok(license);
     }
-
     @PutMapping
     public ResponseEntity<String> updateLicense(
             @PathVariable("organizationId") String organizationId,
